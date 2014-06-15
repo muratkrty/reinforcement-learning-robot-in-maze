f75ee104365a8013c327b8dfd482df5f66c7a550
muratkrty <muratkrty@gmail.com>
2014-06-15 03:58:06 +0300

    Added: create_random_maze.m

---
+++ create_random_maze.m
@@ -0,0 +1,21 @@
+% Author: Murat Kirtay, M.A. Zamani
+% Date: 27/05/2014
+% Description: Create maze matrix and locate random obstacles.
+% Bugs: No known.
+
+function [ maze ] = create_random_maze(N, r)
+
+maze_temp = zeros(N, N);
+
+for i=1:N
+    for j=1:N
+        if rand < 0.01 * r
+            maze_temp(i,j) = 1;
+        end
+    end
+end
+
+maze = maze_temp;
+
+end
+
